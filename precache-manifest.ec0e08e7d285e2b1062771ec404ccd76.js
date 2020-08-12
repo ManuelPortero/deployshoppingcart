fa4ad8577efb0be4d855d444d5e1f7bf97fec44a
ManuelPortero <manuel.portero.leiva@gmail.com>
2020-08-12 19:19:18 +0100

    Updates

---
+++ precache-manifest.ec0e08e7d285e2b1062771ec404ccd76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c47dcc8244f0933ff21a76a35920c9c",
+    "url": "/deployshoppingcart/index.html"
+  },
+  {
+    "revision": "42492a9efbefc8294f98",
+    "url": "/deployshoppingcart/static/css/main.b669ad73.chunk.css"
+  },
+  {
+    "revision": "7c3e0e6432ceb1161fe7",
+    "url": "/deployshoppingcart/static/js/2.4d6709b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployshoppingcart/static/js/2.4d6709b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42492a9efbefc8294f98",
+    "url": "/deployshoppingcart/static/js/main.3f96a39e.chunk.js"
+  },
+  {
+    "revision": "43826982a7f4d13fe8d5",
+    "url": "/deployshoppingcart/static/js/runtime-main.0f8e983c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/deployshoppingcart/precache-manifest.ec0e08e7d285e2b1062771ec404ccd76.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deployshoppingcart/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
